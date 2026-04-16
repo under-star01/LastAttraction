fa6d2409c93462412fa68a4fd8897a428aa0ac0b
1Q <bsc6260@gmail.com>
2026-04-16 13:02:49 +0900

    0416_test2

--- Assets/3.Script/Survivor/SurvivorActionState.cs
+++ Assets/3.Script/Survivor/SurvivorActionState.cs
@@ -189,4 +189,11 @@ public class SurvivorActionState : NetworkBehaviour
         if (currentAction == SurvivorAction.Stunned)
             currentAction = SurvivorAction.None;
     }
+
+    [Server]
+    public void ForceResetActionServer()
+    {
+        // 피격 등으로 인해 현재 진행 중인 강제 행동(트랩 등)을 서버에서 즉시 종료시킴
+        currentAction = SurvivorAction.None;
+    }
 }
