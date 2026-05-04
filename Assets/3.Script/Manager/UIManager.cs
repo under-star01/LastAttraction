d321ba532e8a013f501e68ed86fba44892a04dc3
Under_Star <pktom2@naver.com>
2026-05-04 12:59:53 +0900

    [0504 준비 상태 적용중]

--- Assets/3.Script/Manager/UIManager.cs
+++ Assets/3.Script/Manager/UIManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,11 +6,21 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
 
-    [Header("Buttons")]
+    [Header("Role Select Buttons")]
     [SerializeField] private Button killerButton;
     [SerializeField] private Button survivorButton;
+    [SerializeField] private Button returnButton;
+
+    [Header("Start Game UI")]
+    [SerializeField] private GameObject startGameUI;
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button readyButton;
+    [SerializeField] private TMP_Text readyCntText;
+
+    [Header("Loading UI")]
     [SerializeField] private GameObject loadingPanel;
-    [SerializeField] private GameObject startButton;
+
+    private bool isReady;
 
     private void Awake()
     {
@@ -22,6 +33,8 @@ public class UIManager : MonoBehaviour
         Instance = this;
 
         BindButtons();
+        ShowRoleSelectUI();
+        ShowLoading(false);
     }
 
     private void BindButtons()
@@ -36,6 +49,24 @@ public class UIManager : MonoBehaviour
         {
             survivorButton.onClick.RemoveAllListeners();
             survivorButton.onClick.AddListener(OnClickConnectSurvivor);
+        }
+
+        if (returnButton != null)
+        {
+            returnButton.onClick.RemoveAllListeners();
+            returnButton.onClick.AddListener(OnClickBackButton);
+        }
+
+        if (startButton != null)
+        {
+            startButton.onClick.RemoveAllListeners();
+            startButton.onClick.AddListener(OnClickStartButton);
+        }
+
+        if (readyButton != null)
+        {
+            readyButton.onClick.RemoveAllListeners();
+            readyButton.onClick.AddListener(OnClickReadyButton);
         }
     }
 
@@ -66,7 +97,36 @@ public class UIManager : MonoBehaviour
         if (CustomNetworkManager.Instance == null)
             return;
 
+        isReady = false;
+        UpdateReadyButtonView();
+
         CustomNetworkManager.Instance.BackToRoleSelect();
+        ShowRoleSelectUI();
+    }
+
+    private void OnClickReadyButton()
+    {
+        if (CustomNetworkManager.Instance == null)
+        {
+            Debug.LogError("[UIManager] CustomNetworkManager Instance가 없습니다.");
+            return;
+        }
+
+        isReady = !isReady;
+
+        CustomNetworkManager.Instance.RequestSurvivorReady(isReady);
+        UpdateReadyButtonView();
+    }
+
+    private void OnClickStartButton()
+    {
+        if (CustomNetworkManager.Instance == null)
+        {
+            Debug.LogError("[UIManager] CustomNetworkManager Instance가 없습니다.");
+            return;
+        }
+
+        CustomNetworkManager.Instance.RequestStartGame();
     }
 
     public void ShowLoading(bool isActive)
@@ -75,9 +135,96 @@ public class UIManager : MonoBehaviour
             loadingPanel.SetActive(isActive);
     }
 
-    public void DisableCanvas()
+    public void ShowRoleSelectUI()
+    {
+        SetButtonActive(killerButton, true);
+        SetButtonActive(survivorButton, true);
+        SetButtonActive(returnButton, false);
+
+        if (startGameUI != null)
+            startGameUI.SetActive(false);
+
+        SetButtonActive(startButton, false);
+        SetButtonActive(readyButton, false);
+
+        SetReadyCntActive(false);
+        SetStartButtonInteractable(false);
+
+        isReady = false;
+        UpdateReadyButtonView();
+        SetLobbyReadyCount(0, 0);
+    }
+
+    public void ShowKillerLobbyUI()
+    {
+        SetButtonActive(killerButton, false);
+        SetButtonActive(survivorButton, false);
+        SetButtonActive(returnButton, true);
+
+        if (startGameUI != null)
+            startGameUI.SetActive(true);
+
+        SetButtonActive(startButton, true);
+        SetButtonActive(readyButton, false);
+        SetReadyCntActive(true);
+
+        SetStartButtonInteractable(false);
+    }
+
+    public void ShowSurvivorLobbyUI()
+    {
+        SetButtonActive(killerButton, false);
+        SetButtonActive(survivorButton, false);
+        SetButtonActive(returnButton, true);
+
+        if (startGameUI != null)
+            startGameUI.SetActive(true);
+
+        SetButtonActive(startButton, false);
+        SetButtonActive(readyButton, true);
+        SetReadyCntActive(true);
+
+        isReady = false;
+        UpdateReadyButtonView();
+    }
+
+    public void SetStartButtonInteractable(bool value)
     {
         if (startButton != null)
-            startButton.SetActive(false);
+            startButton.interactable = value;
+    }
+
+    public void SetLobbyReadyCount(int readyCount, int survivorCount)
+    {
+        if (readyCntText != null)
+            readyCntText.text = $"{readyCount}/{survivorCount}";
+    }
+
+    private void SetButtonActive(Button button, bool isActive)
+    {
+        if (button != null)
+            button.gameObject.SetActive(isActive);
+    }
+
+    private void SetReadyCntActive(bool isActive)
+    {
+        if (readyCntText != null)
+            readyCntText.gameObject.SetActive(isActive);
+    }
+
+    private void UpdateReadyButtonView()
+    {
+        if (readyButton == null)
+            return;
+
+        TMP_Text text = readyButton.GetComponentInChildren<TMP_Text>();
+
+        if (text != null)
+            text.text = isReady ? "READY" : "READY?";
+    }
+
+    public void DisableCanvas()
+    {
+        ShowRoleSelectUI();
     }
 }
